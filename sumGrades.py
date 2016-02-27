31354c722ebc04ac2f0c62cbcc9ba2d67b159e1f
Kaelin <kae7in@gmail.com>
2016-02-27 12:22:03 -0600

    Added an option to search all students in specified path rather than a list of eids

--- sumGrades.py
+++ sumGrades.py
@@ -12,51 +12,62 @@ The result is an output pairing each student's eid with their overall grade. Thi
 helps avoid having to do hand calculations.
 """
 
-
 ###############################################################
 # Set this to the path of all student directories
-pathToAllStudents = "/absolute/path/to/students/..."
+#pathToAllStudents = "/absolute/path/to/students/..."
+pathToAllStudents = '/Users/Kaelin/Desktop/students'
 
-# Set this to the list of students to be graded
+# Search all students in 'pathToAllStudents' directory
+searchAllStudents = False
+
+# Set this to the list of students to be graded (used if searchAllStudents == False)
 studentsToBeGraded = "exampleEID1_exampleEID2/exampleEID3_exampleEID4/.../"
 
 # Set the line numbers (0-indexed) of the feedback.txt file with the actual scores to sum
 gradeLines = [0, 2, 6, 7, 9, 10, 11, 13, 16, 17]
 ###############################################################
 
-regexResult = re.findall(r'[a-z]+[0-9]+', studentsToBeGraded)
-studentGrades = {}
-for eid in regexResult:
-	#open student's feedback.txt file
-	feedbackPath = pathToAllStudents + '/' + eid + '/feedback.txt'
-	try:
-		feedbackFile = open(feedbackPath)
+def main():
+	if searchAllStudents:
+		targetStudents = [studentDirectory[0].split('/')[-1] for studentDirectory in os.walk(pathToAllStudents)]
+		targetStudents.pop(0)
+	else:
+		targetStudents = re.findall(r'[a-z]+[0-9]+', studentsToBeGraded)
+	studentGrades = {}
+	for eid in targetStudents:
+		#open student's feedback.txt file
+		feedbackPath = pathToAllStudents + '/' + eid + '/feedback.txt'
 		try:
-			feedbackLines = feedbackFile.readlines()
-		finally:
-			feedbackFile.close()
-	except IOError:
-		print("Warning: can\'t find file for path: " + feedbackPath)
-		continue
-
-	# sum grades
-	gradeTotal = 0
-	for num in gradeLines:
-		line = feedbackLines[num]
-		line = line.split(':')[-1]
-		if not line:
+			feedbackFile = open(feedbackPath)
+			try:
+				feedbackLines = feedbackFile.readlines()
+			finally:
+				feedbackFile.close()
+		except IOError:
+			print("Warning: can\'t find file for path: " + feedbackPath)
 			continue
 
-		# this robust regex line protects against weird typos or spacing errors
-		# it only grabs the last integer on the line specified (the specified grade)
-		grade = re.match(r'^(.*?)(\d+)(\D*)$', line)
-		if not grade:
-			continue
-		gradeTotal += int(grade.group(2))
+		# sum grades
+		gradeTotal = 0
+		for num in gradeLines:
+			line = feedbackLines[num]
+			line = line.split(':')[-1]
+			if not line:
+				continue
+
+			# this robust regex line protects against weird typos or spacing errors
+			# it only grabs the last integer on the line specified (the specified grade)
+			grade = re.match(r'^(.*?)(\d+)(\D*)$', line)
+			if not grade:
+				continue
+			gradeTotal += int(grade.group(2))
 		
-	# place in dictionary
-	studentGrades[eid] = gradeTotal
+		# place in dictionary
+		studentGrades[eid] = gradeTotal
 
-if studentGrades:
-        for student, grade in studentGrades.iteritems():
-                print(student + " " + str(grade))
+	if studentGrades:
+        	for student, grade in studentGrades.iteritems():
+                	print(student + " " + str(grade))
+
+if __name__ == "__main__":
+	main()
